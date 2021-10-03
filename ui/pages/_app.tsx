45b585824eb6d7acfef09b50ee982c6426d67f7c
Ambrose Chua <ambrose@hey.com>
2021-10-03 21:33:09 +0800

    feat: Add bootstrap CSS

--- ui/pages/_app.tsx
+++ ui/pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css';
 import type {AppProps} from 'next/app';
+import Head from 'next/head';
 
 const MyApp = ({Component, pageProps}: AppProps) => (
-	<Component {...pageProps}/>
+	<>
+		<Head>
+			<link
+				rel="stylesheet"
+				href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"
+				integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU"
+				crossOrigin="anonymous"
+			/>
+		</Head>
+		<Component {...pageProps}/>
+	</>
 );
 
 export default MyApp;
