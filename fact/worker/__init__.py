b0cf2460a826ea79b39f5fa8ddc0d11881e3fe58
Ambrose Chua <ambrose@hey.com>
2021-10-06 23:25:32 +0800

    feat: Implement ping-pong action

--- fact/worker/__init__.py
+++ fact/worker/__init__.py
@@ -1,8 +1,21 @@
+import asyncio
 import logging
+from typing import AsyncIterable
 
 from grpc.aio import insecure_channel
-from ..controller_pb2 import HelloRequest, HelloReply
-from ..controller_pb2_grpc import GreeterStub
+from ..controller_pb2 import (
+    SessionResults,
+    SessionEvents,
+    WorkerRegistration,
+    WorkerTaskResult,
+    TaskNoneResult,
+)
+from ..controller_pb2_grpc import WorkerTasksStub
+from ..utils.itertools import chain
+from .stream import Stream
+
+
+log = logging.getLogger("worker")
 
 
 class Worker:
@@ -13,9 +26,38 @@ class Worker:
 
     async def start(self):
         async with insecure_channel(self.controller_addr) as channel:
-            stub = GreeterStub(channel)
-            response: HelloReply = await stub.SayHello(HelloRequest(name="worker"))
-            logging.info(f"Recieved {response.message}")
+            stub = WorkerTasksStub(channel)
+
+            first_result = SessionResults(
+                worker_registration=WorkerRegistration(
+                    previous_uuid=b"TODO", hostname="todo"
+                )
+            )
+
+            response_stream: Stream[SessionResults] = Stream()
+            responses: AsyncIterable[SessionResults] = chain(
+                [
+                    first_result,
+                ],
+                response_stream,
+            )
+            session_events: AsyncIterable[SessionEvents] = stub.Session(responses)
+
+            first_event = await session_events.__aiter__().__anext__()
+            if first_event.WhichOneof("event") != "worker_acceptance":
+                return
+
+            async for session_event in session_events:
+                worker_task = session_event.worker_task
+                log.debug(worker_task)
+
+                await asyncio.sleep(1)
+
+                await response_stream.add(
+                    SessionResults(
+                        worker_task_result=WorkerTaskResult(task_none=TaskNoneResult())
+                    )
+                )
 
     async def stop(self):
         pass
