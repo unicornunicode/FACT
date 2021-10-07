94f7acb2bed9f47f0f48c8e7a35b856fe52af2eb
Ambrose Chua <ambrose@hey.com>
2021-10-07 12:33:43 +0800

    fix: Show more of the mkcommit command

--- tools/commit-msg.py
+++ tools/commit-msg.py
@@ -8,7 +8,7 @@ from utils import check
 if __name__ == "__main__":
     with open(argv[1], "r") as f:
         commit_msg = quote(f.read())
-    check(f"poetry run mkcommit -x {commit_msg} --autoselect", truncate=19)
+    check(f"poetry run mkcommit --autoselect -x {commit_msg}", truncate=36)
 
 
 # vim: set et ts=4 sw=4:
