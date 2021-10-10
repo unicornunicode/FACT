6ed180ed0d57204b95875f223d013d711cf7658b
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2021-10-11 00:31:03 +0800

    refactor: Change in storage folder structure

--- fact/storage/__init__.py
+++ fact/storage/__init__.py
@@ -1,88 +1,102 @@
 import logging
-
-from pathlib import Path
-from shutil import copy
 import os
 import errno
 
+from pathlib import Path
+
 from enum import Enum
 from uuid import UUID
-from time import time
 
 DEFAULT_PATH = Path("/var/lib/fact")
 
 
+class ArtifactType(Enum):
+    unknown = 0
+    disk = 1
+    memory = 2
+
+
 class DataType(Enum):
+    unknown = 0
     full = 1
     partition = 2
     process = 3
 
 
 class Artifact:
-    def __init__(self, src_path: Path, dst_path: Path, file_type: DataType):
-        self.file_name = dst_path.name
-        self.file_type = file_type
-        Artifact.write_to_fs(src_path, dst_path)
+    def __init__(
+        self,
+        artifact_name: str,
+        artifact_type: ArtifactType = ArtifactType.unknown,
+        sub_type: DataType = DataType.unknown,
+    ):
+        self.artifact_name = artifact_name
+        self.artifact_type = artifact_type
+        self.sub_type = sub_type
 
-    @staticmethod
-    def write_to_fs(src_path: Path, dst_path: Path):
-        if not src_path.exists():
-            raise OSError(errno.ENOENT, os.strerror(errno.ENOENT), src_path.resolve())
-        copy(src_path, dst_path)
+    def get_artifact_type(self):
+        return self.artifact_type.name
+
+    def get_sub_type(self):
+        return self.sub_type.name
+
+    def get_artifact_path(self):
+        artifact_type = self.get_artifact_type()
+        sub_type = self.get_sub_type()
+        return Path(artifact_type) / Path(sub_type) / Path(self.artifact_name)
+
+    def get_artifact_info(self):
+        sub_type = self.get_sub_type()
+        return {"artifact_name": self.artifact_name, "sub_type": sub_type}
 
 
 class Disk(Artifact):
-    def __init__(self, src_path: Path, dst_path: Path, file_type: DataType):
-        super().__init__(src_path, dst_path, file_type)
+    artifact_type: ArtifactType = ArtifactType.disk
+
+    def __init__(self, artifact_name: str, sub_type: DataType = DataType.unknown):
+        super().__init__(artifact_name, Disk.artifact_type, sub_type)
 
 
 class Memory(Artifact):
-    def __init__(self, src_path: Path, dst_path: Path, file_type: DataType):
-        super().__init__(src_path, dst_path, file_type)
+    artifact_type: ArtifactType = ArtifactType.memory
+
+    def __init__(self, artifact_name: str, sub_type: DataType = DataType.unknown):
+        super().__init__(artifact_name, Memory.artifact_type, sub_type)
 
 
-class Timecapsule:
-    def __init__(self, timestamp: time):
-        self.timestamp = timestamp
+class Task:
+    def __init__(self, task_uuid: UUID):
+        self.task_uuid = task_uuid
         self.disks: list[Disk] = []
         self.memories: list[Memory] = []
 
-    def add_disk(self, disk_path: Path, dst_path: Path, file_type: DataType):
-        disk = Disk(disk_path, dst_path, file_type)
+    def get_task_uuid(self):
+        return str(self.task_uuid)
+
+    def add_disk(self, disk_name: str, artifact_type: DataType):
+        disk = Disk(disk_name, artifact_type)
         self.disks.append(disk)
 
-    def add_memory(self, memory_path: Path, dst_path: Path, file_type: DataType):
-        memory = Memory(memory_path, dst_path, file_type)
+    def add_memory(self, disk_name: str, artifact_type: DataType):
+        memory = Memory(disk_name, artifact_type)
         self.memories.append(memory)
 
-    def get_artifacts(self):
-        timecapsule = {"disks": [], "memories": []}
+    def get_task_path(self):
+        task_uuid = self.get_task_uuid()
+        return Path(task_uuid)
+
+    def _get_artifacts(self):
+        artifacts = {"disk": [], "memory": []}
         for disk in self.disks:
-            timecapsule["disks"].append(disk.file_name)
+            artifacts["disk"].append(disk.get_artifact_info())
         for mem in self.memories:
-            timecapsule["memories"].append(mem.file_name)
-        return timecapsule
+            artifacts["memory"].append(mem.get_artifact_info())
+        return artifacts
 
-
-class Target:
-    def __init__(self, storage_dir: Path, target_uuid: UUID):
-        self.storage_dir = storage_dir
-        self.target_uuid = target_uuid
-        self.target_dir = storage_dir / str(target_uuid)
-        if not self.target_dir.exists():
-            self.target_dir.mkdir(parents=True, exist_ok=True)
-        self.timecapsules: list[Timecapsule] = []
-
-    def is_timecapsule_exists(self, timecapsule: Timecapsule):
-        target_timestamp = timecapsule.timestamp
-        for t in self.timecapsules:
-            if t.timestamp == target_timestamp:
-                return True
-        return False
-
-    def add_timecapsule(self, timecapsule: Timecapsule):
-        if not self.is_timecapsule_exists(timecapsule):
-            self.timecapsules.append(timecapsule)
+    def get_task_info(self):
+        task_uuid = self.get_task_uuid()
+        artifacts = self._get_artifacts()
+        return {"task_uuid": task_uuid, "artifacts": artifacts}
 
 
 class Storage:
@@ -90,27 +104,26 @@ class Storage:
         if not data_dir.exists():
             data_dir.mkdir(parents=True, exist_ok=True)
         self.data_dir = data_dir
-        self.targets: list[Target] = []
+        self.tasks: list[Task] = []
 
-    def is_target_exists(self, target: Target):
-        target_uuid = target.target_uuid
-        for t in self.targets:
-            if t.target_uuid == target_uuid:
+    def is_task_exists(self, task: Task):
+        task_uuid = task.task_uuid
+        for t in self.tasks:
+            if t.task_uuid == task_uuid:
                 return True
         return False
 
-    def add_target(self, target: Target):
-        if not self.is_target_exists(target):
-            self.targets.append(target)
+    def add_task(self, task: Task):
+        if self.is_task_exists(task):
+            pass  # Raise custom exception
+        self.tasks.append(task)
 
-    def get_filesystem(self):
-        fs = dict()
-        for t in self.targets:
-            target = dict()
-            for tc in t.timecapsules:
-                target[tc.timestamp] = tc.get_artifacts()
-            fs[t.target_uuid] = target
-        return fs
+    def get_storage_path(self):
+        return self.data_dir
+
+    def get_filesystem_info(self):
+        tasks = [task.get_task_info() for task in self.tasks]
+        return {"data_dir": self.data_dir, "tasks": tasks}
 
     @classmethod
     def duplicate_storage_from_dict(cls, storage_dict: dict, new_data_dir: Path):
