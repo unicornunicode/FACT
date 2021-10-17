05449779dde78f738372baf2bec13dc8ee802038
Ambrose Chua <ambrose@hey.com>
2021-10-17 19:56:47 +0800

    feat: Implement initial UI

---
+++ ui/features/config.ts
@@ -0,0 +1,22 @@
+import process from 'process';
+
+export type Config = {
+	management: {
+		endpoint: string;
+		options: {
+			debug?: boolean;
+		};
+	};
+};
+
+export async function readConfig(): Promise<Config> {
+	if (typeof window !== 'undefined') {
+		const response = await fetch('/api/config');
+		const config = await response.json() as Config;
+		return config;
+	}
+
+	return {
+		management: {endpoint: process.env.MANAGEMENT_ENDPOINT ?? 'http://localhost:5124', options: {debug: true}},
+	};
+}
