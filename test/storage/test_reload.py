a775e4872263d8bd19a66efde154c463d767cd53
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2021-10-27 01:25:53 +0800

    wip: Updated test case for reload

--- test/storage/test_reload.py
+++ test/storage/test_reload.py
@@ -1,8 +1,9 @@
-from fact.storage import Storage, Task
+from fact.storage import Storage, Task, Artifact
 
 from pathlib import Path
 from tempfile import mkdtemp
 from uuid import UUID
+from shutil import rmtree
 
 
 def test_init_storage():
@@ -11,19 +12,23 @@ def test_init_storage():
     desired_path.rmdir()
 
     id = str(UUID(int=0))
+    artf1 = Artifact("sda.raw", "disk")
+    artf1_contents = b"I am sda.raw"  # noqa: F841
 
     stg1 = Storage(desired_path)
     assert stg1.get_storage_path() == desired_path
     assert stg1.get_storage_info() == {"data_dir": str(desired_path), "tasks": []}
     stg1.add_task(Task(id))
+    stg1.add_task_artifact(id, artf1)  # Use Session to add artifact
 
     # Storage should be able to run with the same folder
     stg2 = Storage(desired_path)  # noqa: F841
-    # TODO: Fix the code to satisfy this test
+    # TODO: Fix the code to satisfy these tests
     # assert stg2.get_task(id) is not None
+    # assert stg2.get_task_artifact_path(id, artf1) is not None
 
     # Cleanup
-    desired_path.rmdir()
+    rmtree(desired_path, ignore_errors=True)
 
 
 # vim: set et ts=4 sw=4:
