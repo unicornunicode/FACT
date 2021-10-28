8267ce9309d83bec6b141bbab6a836684c1b1bcd
annoy-o-mus <55198236+annoy-o-mus@users.noreply.github.com>
2021-10-28 08:00:58 +0800

    refactor: change filepath from os/str to pathlib.Path

--- fact/utils/hashing.py
+++ fact/utils/hashing.py
@@ -1,7 +1,9 @@
 import hashlib
 
+from pathlib import Path
 
-def calculate_sha256(filepath: str) -> str:
+
+def calculate_sha256(filepath: Path) -> str:
     sha256_hash = hashlib.sha256()
     with open(filepath, "rb") as f:
         for byte_block in iter(lambda: f.read(8192), b""):
--- test/test_misc.py
+++ test/test_misc.py
@@ -1,13 +1,14 @@
 import os
 
+from pathlib import Path
+
 from fact.utils.decompression import decompress_gzip
 from fact.exceptions import GzipDecompressionError
-
-from .utils.hashing import calculate_sha256
+from fact.utils.hashing import calculate_sha256
 
 
 def test_sha256sum_implementation():
-    hashstring = calculate_sha256("test/files/invalid_gzip.gz")
+    hashstring = calculate_sha256(Path("test/files/invalid_gzip.gz"))
     assert (
         hashstring.lower()
         == "5752c3c283912fb6cf27984849003e793cc6ce42f0416ecfcb91d24e5c60f7a8"
@@ -18,13 +19,13 @@ def test_invalid_gunzip():
     assert_list = []
 
     try:
-        decompress_gzip("test/files/raw_data")
+        decompress_gzip(Path("test/files/raw_data"))
     except GzipDecompressionError as e:
         if "no .gz extension" in e.message:
             assert_list.append(1)
 
     try:
-        decompress_gzip("test/files/invalid_gzip.gz")
+        decompress_gzip(Path("test/files/invalid_gzip.gz"))
     except GzipDecompressionError as e:
         if "Error reading/processing" in e.message:
             assert_list.append(2)
@@ -35,9 +36,9 @@ def test_invalid_gunzip():
 
 
 def test_gunzip():
-    decompress_gzip("test/files/test_data.gz")
-    assert calculate_sha256("test/files/test_data") == calculate_sha256(
-        "test/files/raw_data"
+    decompress_gzip(Path("test/files/test_data.gz"))
+    assert calculate_sha256(Path("test/files/test_data")) == calculate_sha256(
+        Path("test/files/raw_data")
     )
     # cleanup
     os.remove("test/files/test_data")
