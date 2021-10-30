08f17a2e789e6d791ed3f84170d3149499aaab99
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2021-10-30 18:38:05 +0800

    feat: Added disk setting up and mounting APIs

--- fact/ingestion/analyse.py
+++ fact/ingestion/analyse.py
@@ -1,21 +1,23 @@
-from fact.storage import Artifact
+from fact.utils.decompression import decompress_gzip
+from fact.utils.hashing import calculate_sha256
 
-# from fact.utils.decompression import decompress_gzip
-
+from subprocess import Popen, PIPE
 from pathlib import Path
 from tempfile import mkstemp
+import re
+
+from typing import List
 
 
 class Analyzer:
     """Base class for all analyzers"""
 
     def __init__(
-        self, artifact_path: Path, artifact: Artifact, artifact_hash: str
+        self, sudo_password: str, artifact_path: Path, artifact_hash: str
     ) -> None:
+        self.sudo_password = sudo_password
         self.decompress_path: Path
         self.artifact_path = artifact_path
-        self.artifact_type = artifact.get_artifact_type()
-        self.artifact_name = artifact.artifact_name
         if self.hash_integrity_check(artifact_hash):
             self.gzip_decompress()
 
@@ -23,51 +25,152 @@ class Analyzer:
         artifact_suffix = "".join(self.artifact_path.suffixes[:-1])
         _, decompress_path = mkstemp(artifact_suffix, self.artifact_path.stem)
         self.decompress_path = Path(decompress_path)
-        self.decompress_path.unlink()  # let decompress_gzip create file
-        # decompress_gzip(
-        #     self.artifact_path, self.decompress_path, True
-        # )  # Include exception handling
+        self.decompress_path.unlink()
+        decompress_gzip(
+            self.artifact_path, self.decompress_path, True
+        )  # Include exception handling
 
-    def hash_integrity_check(self, file_hash: str) -> bool:
-        # Move and use sha256 util from test/utils
-        return True
+    def hash_integrity_check(self, expected_hash: str) -> bool:
+        actual_hash = calculate_sha256(self.artifact_path)
+        return actual_hash == expected_hash
 
 
 class DiskAnalyzer(Analyzer):
-    def __init__(self, artifact_path: Path, artifact: Artifact, artifact_hash: str):
-        super().__init__(artifact_path, artifact, artifact_hash)
+    def __init__(self, sudo_password: str, disk_image_path: Path, artifact_hash: str):
+        super().__init__(sudo_password, disk_image_path, artifact_hash)
+        self.loop_device_path: Path
+        self.mount_paths: List[Path]
+        self.file_systems: List
 
     def __enter__(self):
         self.setup()
         return self
 
     def __exit__(self):
-        pass
+        self.cleanup()
 
     def setup(self):
-        pass  # setup the disk image
+        self._setup_loop_device()
+        self._identify_file_systems()
+        self._mount_file_systems()
 
-    @staticmethod
-    def attach_loop_device(disk_path: Path) -> Path:
-        loop_device_path: Path = Path()
-        # create loop device with losetup
-        return loop_device_path  # loop device path (/dev/loopXX)
+    def cleanup(self):
+        self._unmount_file_systems()
+        self._detach_loop_device()
 
-    @staticmethod
-    def identify_partitions(loop_device: Path) -> dict:
-        partitions: dict = dict()
-        # get all paritions with lsblk
-        return partitions
+    def analyse(self):
+        return self._traverse_file_systems()
 
-    @staticmethod
-    def mount_partition(partition: str, loop_device: Path) -> Path:
-        mounted_path: Path = Path()
-        # mount parition of loop device
-        return mounted_path  # mounted path
+    def _setup_loop_device(self):
+        cmd = [
+            "sudo",
+            "-S",
+            "losetup",
+            "--find",
+            "--show",
+            "--read-only",
+            self.disk_image_path,
+        ]
+        with Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE) as proc:
+            stdout, stderr = proc.communicate(self.sudo_password.encode())
+        if not stderr.decode():
+            self.loop_device_path = stdout.decode().strip()
+        else:
+            pass  # raise exception
 
-    @staticmethod
-    def traverse_file_system(self, mount_path: Path) -> dict:
-        file_system_info: dict = dict()
-        # traverse with pathlib.Path.rglob("*")
+    def _detach_loop_device(self):
+        cmd = ["sudo", "-S", "losetup", "--detach", self.loop_device_path]
+        with Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE) as proc:
+            _, stderr = proc.communicate(self.sudo_password.encode())
+        if not stderr.decode():
+            return
+        else:
+            pass  # raise exception
+
+    def _identify_file_systems(self):
+        sector_size = 512
+        cmd = [
+            "sudo",
+            "-S",
+            "fdisk",
+            "--list",
+            "--sector-size",
+            str(sector_size),
+            self.loop_device_path,
+        ]
+        with Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE) as proc:
+            stdout, stderr = proc.communicate(self.sudo_password.encode())
+        if not stderr.decode():
+            fdisk_output = stdout.decode().strip()
+            partition_table = re.search(
+                r"Device\s*Start\s*End\s*Sectors\s*Size\s*Type\s*([.\S\W]*)",
+                fdisk_output,
+            )
+
+            self.file_systems = []
+            if partition_table:
+                records = partition_table.group(1).split("\n")
+                for record in records:
+                    partition = re.split(r"\s+", record)
+                    partition_columns = len(partition)
+                    if partition_columns == 1:
+                        break
+                    elif partition_columns > 5:
+                        diff = 5 - partition_columns
+                        partition = partition[:diff] + " ".join(partition[diff:])
+
+                    if partition[-1] == "Linux filesystem":
+                        start_sector = int(partition[1])
+                        self.file_systems.append(str(start_sector * sector_size))
+                    # TODO: Add more types
+        else:
+            pass  # raise exception
+
+    def _mount_file_systems(self):
+        try:
+            self.file_systems
+        except AttributeError:
+            raise
+
+        mnt_base_path = "/mnt/diskartf"
+        for idx, fs in enumerate(self.file_systems):
+            fs_mnt_path = Path(mnt_base_path + str(idx))
+            if not fs_mnt_path.exists():
+                fs_mnt_path.mkdir(parents=True)
+            cmd = [
+                "sudo",
+                "-S",
+                "mount",
+                "--options",
+                f"offset={fs},ro,noload",
+                self.loop_device_path,
+                fs_mnt_path,
+            ]
+            with Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE) as proc:
+                _, stderr = proc.communicate(self.sudo_password.encode())
+            if not stderr.decode():
+                self.mount_paths.append(fs_mnt_path)
+                continue
+            else:
+                pass  # raise exception / log error
+
+    def _unmount_file_systems(self):
+        try:
+            self.mount_paths
+        except AttributeError:
+            raise
+
+        for path in self.mount_paths:
+            cmd = ["sudo", "-S", "umount", path]
+            with Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE) as proc:
+                _, stderr = proc.communicate(self.sudo_password.encode())
+            if not stderr.decode():
+                continue
+            else:
+                pass  # raise exception / log error
+
+    def _traverse_file_systems(self):
+        file_system_info = dict()
+        # TODO: Traverse with pathlib.Path.rglob("*") and
         # get all info on all files and dirs with pathlib.Path.lstat
         return file_system_info
