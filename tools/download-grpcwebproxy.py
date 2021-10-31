66b6bf269f75f2fbb4afcdcbc0d509c45a2be5a3
Ambrose Chua <ambrose@hey.com>
2021-10-31 23:58:47 +0800

    refactor: Shared archive downloading utilities

--- tools/download-grpcwebproxy.py
+++ tools/download-grpcwebproxy.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 
-from typing import Generator
-from urllib.request import urlopen
-from tempfile import TemporaryDirectory
 from zipfile import ZipFile
-from pathlib import Path
+
+from utils import root, download_temporary, copy
 
 VERSION = "0.14.1"
 PLATFORM = "linux-x86_64"
@@ -12,39 +10,21 @@ URL = (
     "https://github.com/improbable-eng/grpc-web"
     f"/releases/download/v{VERSION}/grpcwebproxy-v{VERSION}-{PLATFORM}.zip"
 )
-BIN = Path("fact", "grpcwebproxy", "grpcwebproxy")
-
-
-def copy(dst, src) -> Generator[int, None, None]:
-    counter = 0
-    while buf := src.read(8192):
-        counter += len(buf)
-        dst.write(buf)
-        yield counter
+BIN = root / "fact" / "grpcwebproxy" / "grpcwebproxy"
 
 
 if __name__ == "__main__":
-    try:
-        with TemporaryDirectory() as tmpdir:
-            zippath = Path(tmpdir, "grpcwebproxy.zip")
-            with open(zippath, "wb") as downloadfile, urlopen(URL) as request:
-                size = request.headers["Content-Length"]
-                for counter in copy(downloadfile, request):
-                    print(f"\rdownload: {counter} / {size}", end="")
+    with download_temporary(URL) as archivepath:
+        with ZipFile(archivepath) as archive:
+            for info in archive.infolist():
+                if "grpcwebproxy" in info.filename:
+                    break
+            with archive.open(info.filename) as zipbin, open(BIN, "wb") as file:
+                size = info.file_size
+                for counter in copy(file, zipbin):
+                    print(f"\rextract: {counter} / {size}", end="")
                 print()
-            with ZipFile(zippath) as zipfile:
-                for info in zipfile.infolist():
-                    if "grpcwebproxy" in info.filename:
-                        break
-                with zipfile.open(info.filename) as zipbin, open(BIN, "wb") as file:
-                    size = info.file_size
-                    for counter in copy(file, zipbin):
-                        print(f"\rextract: {counter} / {size}", end="")
-                    print()
-        BIN.chmod(0o755)
-    except Exception as e:
-        print(e)
-        exit(1)
+    BIN.chmod(0o755)
 
 
 # vim: set et ts=4 sw=4:
