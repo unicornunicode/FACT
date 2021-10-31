9f7671205c30807c1f21d712777f590fc1d1b9aa
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2021-10-31 23:03:55 +0800

    refactor: Modified Storage module exceptions and added for Ingestion module

--- fact/exceptions.py
+++ fact/exceptions.py
@@ -29,54 +29,59 @@ class GzipDecompressionError(FACTError, ValueError):
         super().__init__(f"Failed to decompress gzipped data at {path}")
 
 
-class StorageModuleException(Exception):
-    """Base class for Storage module exceptions"""
-
-    def __init__(self, message: str, field: str):
-        self.message = message
-        self.field = field
-
-    def __str__(self):
-        return f"{self.field} -> {self.message}"
-
-
-class TaskExistsError(StorageModuleException):
+class TaskExistsError(FACTError, Exception):
     def __init__(self, message: str, task_uuid: str):
         """Exception if Task exists"""
-        super().__init__(message, task_uuid)
+        super().__init__(f"{task_uuid} -> {message}")
 
 
-class TaskNotFoundError(StorageModuleException):
+class TaskNotFoundError(FACTError, Exception):
     def __init__(self, message: str, task_uuid: str):
         """Exception if Task not found"""
-        super().__init__(message, task_uuid)
+        super().__init__(f"{task_uuid} -> {message}")
 
 
-class TaskInvalidUUID(StorageModuleException):
+class TaskInvalidUUID(FACTError, Exception):
     def __init__(self, message: str, task_uuid: str):
         """Exception if Task has invalid UUID"""
-        super().__init__(message, task_uuid)
+        super().__init__(f"{task_uuid} -> {message}")
 
 
-class ArtifactExistsError(StorageModuleException):
+class ArtifactExistsError(FACTError, Exception):
     def __init__(self, message: str, artifact_info: dict):
         """Exception if Artifact exists"""
-        super().__init__(message, str(artifact_info))
+        super().__init__(f"{str(artifact_info)} -> {message}")
 
 
-class ArtifactNotFoundError(StorageModuleException):
+class ArtifactNotFoundError(FACTError, Exception):
     def __init__(self, message: str, artifact_info: dict):
         """Exception if Artifact not found"""
-        super().__init__(message, str(artifact_info))
+        super().__init__(f"{str(artifact_info)} -> {message}")
 
 
-class ArtifactInvalidName(StorageModuleException):
+class ArtifactInvalidName(FACTError, Exception):
     def __init__(self, message: str, artifact_name: str):
         """Exception if Artifact has invalid name"""
-        super().__init__(message, artifact_name)
+        super().__init__(f"{artifact_name} -> {message}")
 
 
-class ArtifactInvalidType(StorageModuleException):
+class ArtifactInvalidType(FACTError, Exception):
     def __init__(self, message: str, artifact_type: str):
         """Exception if Artifact has invalid type"""
-        super().__init__(message, artifact_type)
+        super().__init__(f"{artifact_type} -> {message}")
+
+
+class LoopDeviceSetupError(FACTError, Exception):
+    pass
+
+
+class LoopDeviceDetachError(FACTError, Exception):
+    pass
+
+
+class MountPartitionError(FACTError, Exception):
+    pass
+
+
+class UnmountPartitionError(FACTError, Exception):
+    pass
