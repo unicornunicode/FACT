25a21b1b1f68712fdb6ae28da66be753195ccc25
Ambrose Chua <ambrose@hey.com>
2021-11-03 18:21:15 +0800

    fix: Improve FileRecord format

--- fact/ingestion/record.py
+++ fact/ingestion/record.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
 import os
+from datetime import datetime
 from dataclasses import dataclass
-from typing import Union
+from typing import Optional, Union
 
 
 @dataclass
@@ -17,32 +18,36 @@ class RecordBase:
 class FileRecord(RecordBase):
     """Record class for disk"""
 
+    partition: Optional[str]
+    path: str
     st_mode: int
     st_ino: int
     st_nlink: int
     st_uid: int
     st_gid: int
     st_size: int
-    st_atime: float
-    st_mtime: float
-    st_ctime: float
+    st_atime: datetime
+    st_mtime: datetime
+    st_ctime: datetime
 
     @classmethod
     def from_stat_result(
-        cls, artifact: str, type: str, os_stat: os.stat_result
+        cls, artifact: str, partition: Optional[str], path: str, os_stat: os.stat_result
     ) -> FileRecord:
         return cls(
             fact_artifact=artifact,
-            fact_type=type,
+            fact_type="file",
+            partition=partition,
+            path=path,
             st_mode=os_stat.st_mode,
             st_ino=os_stat.st_ino,
             st_nlink=os_stat.st_nlink,
             st_uid=os_stat.st_uid,
             st_gid=os_stat.st_gid,
             st_size=os_stat.st_size,
-            st_atime=os_stat.st_atime,
-            st_mtime=os_stat.st_mtime,
-            st_ctime=os_stat.st_ctime,
+            st_atime=datetime.fromtimestamp(os_stat.st_atime),
+            st_mtime=datetime.fromtimestamp(os_stat.st_mtime),
+            st_ctime=datetime.fromtimestamp(os_stat.st_ctime),
         )
 
 
--- test/ingestion/test_disk_analyzer.py
+++ test/ingestion/test_disk_analyzer.py
@@ -6,48 +6,41 @@ from pathlib import Path
 
 
 @pytest.fixture
-def init_disk_analyzer():
+def disk_analyzer():
     disk_gz_path = Path("test/files/disk.raw.gz")
     try:
-        dsk_anz = DiskAnalyzer(
-            disk_gz_path,
-            "cfff29e041c818881a58a5ac1ddc68f6fa05b02504485c327822d6df2e131fa5",
+        disk_analyzer = DiskAnalyzer(
+            artifact="test",
+            disk_image_path=disk_gz_path,
+            artifact_hash="cfff29e041c818881a58a5ac1ddc68f6fa05b02504485c327822d6df2e131fa5",
         )
     except PermissionError:
         pytest.skip()
-    return dsk_anz
+    return disk_analyzer
 
 
-def test_init_analyzer(init_disk_analyzer):
-    dsk_anz: DiskAnalyzer = init_disk_analyzer
-    assert dsk_anz.decompress_path
+def test_init_analyzer(disk_analyzer: DiskAnalyzer):
+    assert disk_analyzer.decompress_path
 
 
-def test_setup_disk(init_disk_analyzer):
-    dsk_anz: DiskAnalyzer = init_disk_analyzer
-
+def test_setup_disk(disk_analyzer: DiskAnalyzer):
     # Call each function called in DiskAnalyzer.setup()
 
-    dsk_anz._setup_loop_device()
-    dsk_anz.loop_device_path  # Asserts AttributeError not raised
+    disk_analyzer._setup_loop_device()
+    disk_analyzer._identify_partitions()
+    disk_analyzer._mount_partitions()
 
-    dsk_anz._identify_partitions()
-    dsk_anz.partitions  # Asserts AttributeError not raised
-
-    dsk_anz._mount_partitions()
-    dsk_anz.mount_paths  # Asserts AttributeError not raised
-
-    dsk_anz.cleanup()
+    disk_analyzer.cleanup()
 
 
-def test_cleanup_disk(init_disk_analyzer):
-    dsk_anz: DiskAnalyzer = init_disk_analyzer
-    dsk_anz.setup()
+def test_cleanup_disk(disk_analyzer: DiskAnalyzer):
+    disk_analyzer.setup()
 
     # Call each function called in DiskAnalyzer.cleanup()
 
-    dsk_anz._unmount_partitions()
-    for mp in dsk_anz.mount_paths:
+    disk_analyzer._unmount_partitions()
+    assert disk_analyzer.mount_paths is not None
+    for mp in disk_analyzer.mount_paths:
         assert not mp.exists()
 
-    dsk_anz._detach_loop_device()
+    disk_analyzer._detach_loop_device()
